bb5d8b083e39803ee8ee14bab6a7f696b6d60a1f
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2014-02-01 16:26:36 +0800

    fix load path

--- lib/evil_utils.rb
+++ lib/evil_utils.rb
@@ -3,5 +3,5 @@ require 'bundler'
 Bundler.setup(:default)
 
 require 'rails/generators'
-require 'lib/generators/evil_utils/scaffold_controller/scaffold_controller_generator'
-require 'lib/generators/evil_utils/scaffold_controller_spec/scaffold_controller_generator_spec'
+require 'generators/evil_utils/scaffold_controller/scaffold_controller_generator'
+require 'generators/evil_utils/scaffold_controller_spec/scaffold_controller_generator_spec'
