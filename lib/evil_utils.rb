58d3ea7f9645920e924b9704e636f84a69b42c3e
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2014-02-01 16:38:43 +0800

    load all generators

--- lib/evil_utils.rb
+++ lib/evil_utils.rb
@@ -3,5 +3,4 @@ require 'bundler'
 Bundler.setup(:default)
 
 require 'rails/generators'
-require 'generators/evil_utils/scaffold_controller/scaffold_controller_generator'
-require 'generators/evil_utils/scaffold_controller_spec/scaffold_controller_generator_spec'
+Dir['generators/**/**/*_generator.rb'].each { |g| require g }
