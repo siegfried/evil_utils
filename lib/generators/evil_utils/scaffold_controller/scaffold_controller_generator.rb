be26bd83cdcc06f08669a4f2e6531c6077542b54
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2014-02-25 21:40:06 +0800

    hook_for spec

--- lib/generators/evil_utils/scaffold_controller/scaffold_controller_generator.rb
+++ lib/generators/evil_utils/scaffold_controller/scaffold_controller_generator.rb
@@ -8,6 +8,7 @@ module EvilUtils
     check_class_collision suffix: "Controller"
     class_option :orm, banner: "NAME", type: :string, required: true, desc: "ORM to generate the controller for"
     argument :attributes, type: :array, default: [], banner: "field:type field:type"
+    hook_for :scaffold_controller_spec
 
     def create_controller_files
       template "controller.rb", File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
