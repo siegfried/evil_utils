6e1d44ada16e8f4db15f3d87a7e3b6a5dd74239a
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2014-06-28 21:36:33 +0800

    remove deprecated should

--- lib/evil_utils/version.rb
+++ lib/evil_utils/version.rb
@@ -2,7 +2,7 @@ class EvilUtils
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 6
+    PATCH = 7
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
--- lib/generators/evil_utils/scaffold_controller_spec/templates/controller_spec.rb
+++ lib/generators/evil_utils/scaffold_controller_spec/templates/controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe <%= controller_class_name %>Controller, :type => :controller do
   describe "#index" do
     before { get :index, <%= table_name_params %> }
 
-    specify { assigns(:<%= table_name %>).should eq([<%= file_name %>]) }
+    specify { expect(assigns(:<%= table_name %>)).to eq([<%= file_name %>]) }
     it { should render_template :index }
   end
 
@@ -22,21 +22,21 @@ RSpec.describe <%= controller_class_name %>Controller, :type => :controller do
   describe "#show" do
     before { get :show, <%= file_name_params %> }
 
-    specify { assigns(:<%= ns_file_name %>).should eq(<%= file_name %>) }
+    specify { expect(assigns(:<%= ns_file_name %>)).to eq(<%= file_name %>) }
     it { should render_template :show }
   end
 
   describe "#new" do
     before { get :new, <%= table_name_params %> }
 
-    specify { assigns(:<%= ns_file_name %>).should be_a_new(<%= class_name %>) }
+    specify { expect(assigns(:<%= ns_file_name %>)).to be_a_new(<%= class_name %>) }
     it { should render_template :new }
   end
 
   describe "#edit" do
     before { get :edit, <%= file_name_params %> }
 
-    specify { assigns(:<%= ns_file_name %>).should eq(<%= file_name %>) }
+    specify { expect(assigns(:<%= ns_file_name %>)).to eq(<%= file_name %>) }
     it { should render_template :edit }
   end
 
@@ -46,15 +46,15 @@ RSpec.describe <%= controller_class_name %>Controller, :type => :controller do
     context "with valid params" do
       let(:params) { <%= table_name_params %>.merge(<%= ns_file_name %>: valid_attributes) }
 
-      specify { assigns(:<%= ns_file_name %>).should be_persisted }
+      specify { expect(assigns(:<%= ns_file_name %>)).to be_persisted }
       it { should redirect_to <%= class_name %>.last }
     end
 
     context "with invalid params" do
       let(:params) { <%= table_name_params %>.merge(<%= ns_file_name %>: invalid_attributes) }
 
-      specify { assigns(:<%= ns_file_name %>).should_not be_persisted }
-      specify { assigns(:<%= ns_file_name %>).should_not be_valid }
+      specify { expect(assigns(:<%= ns_file_name %>)).not_to be_persisted }
+      specify { expect(assigns(:<%= ns_file_name %>)).not_to be_valid }
       it { should render_template :new }
     end
   end
@@ -65,16 +65,16 @@ RSpec.describe <%= controller_class_name %>Controller, :type => :controller do
     context "with valid params" do
       let(:params) { <%= file_name_params %>.merge(<%= ns_file_name %>: valid_attributes) }
 
-      specify { assigns(:<%= ns_file_name %>).should eq(<%= file_name %>) }
-      specify { assigns(:<%= ns_file_name %>).should be_persisted }
+      specify { expect(assigns(:<%= ns_file_name %>)).to eq(<%= file_name %>) }
+      specify { expect(assigns(:<%= ns_file_name %>)).to be_persisted }
       it { should redirect_to <%= file_name %> }
     end
 
     context "with invalid params" do
       let(:params) { <%= file_name_params %>.merge(<%= ns_file_name %>: invalid_attributes) }
 
-      specify { assigns(:<%= ns_file_name %>).should eq(<%= file_name %>) }
-      specify { assigns(:<%= ns_file_name %>).should_not be_valid }
+      specify { expect(assigns(:<%= ns_file_name %>)).to eq(<%= file_name %>) }
+      specify { expect(assigns(:<%= ns_file_name %>)).not_to be_valid }
       it { should render_template :edit }
     end
   end
@@ -82,8 +82,8 @@ RSpec.describe <%= controller_class_name %>Controller, :type => :controller do
   describe "#destroy" do
     before { delete :destroy, <%= file_name_params %> }
 
-    specify { assigns(:<%= ns_file_name %>).should eq(<%= file_name %>) }
-    specify { assigns(:<%= ns_file_name %>).should be_destroyed }
+    specify { expect(assigns(:<%= ns_file_name %>)).to eq(<%= file_name %>) }
+    specify { expect(assigns(:<%= ns_file_name %>)).to be_destroyed }
     it { should redirect_to(<%= index_helper %>_url) }
   end
 
